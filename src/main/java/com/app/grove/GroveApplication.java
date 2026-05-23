0f0a43fa2648a15c1ce0f54b6a7548a55d84d240
José Villalobos <jose.villalobos.j@utec.edu.pe>
2026-05-23 16:05:26 -0500

    Estructura inicial

---
+++ src/main/java/com/app/grove/GroveApplication.java
@@ -0,0 +1,13 @@
+package com.app.grove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/grove/GroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.grove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
