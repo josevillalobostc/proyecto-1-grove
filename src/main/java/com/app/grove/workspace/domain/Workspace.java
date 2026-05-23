8e18c87700ffea7da2301cb9225d692e07504ddd
rvcuba <rvl.cuba@gmail.com>
2026-05-23 18:52:05 -0500

    Entity Workspace Created

---
+++ src/main/java/com/app/grove/workspace/domain/Workspace.java
@@ -0,0 +1,42 @@
+package com.app.grove.workspace.domain;
+
+import com.app.grove.concept.domain.Concept;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+@Node
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Workspace {
+
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    private String id;
+
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+
+    @Relationship(
+        type = "CONTAINS",
+        direction = Relationship.Direction.OUTGOING
+    )
+    private List<Concept> concepts;
+
+    @Relationship(
+        type = "MEMBER_OF",
+        direction = Relationship.Direction.INCOMING
+    )
+    private List<User> members;
+}
