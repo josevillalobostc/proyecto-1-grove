be4f813a47e88dff64da07bb74f8ddbf9eba876b
Maxwell <maxwellcollazossolis@gmail.com>
2026-05-24 11:28:15 -0500

    Agregando NewCommentNotificationEvent

--- src/main/java/com/app/grove/events/NewCommentNotificationEvent.java
+++ src/main/java/com/app/grove/events/NewCommentNotificationEvent.java
@@ -1,4 +1,12 @@
 package com.app.grove.events;
 
+import lombok.Value;
+
+@Value
 public class NewCommentNotificationEvent {
+    String conceptId;
+    String conceptTitle;
+    String commentText;
+    String commentAuthorName;
+    String conceptAuthorId;
 }
