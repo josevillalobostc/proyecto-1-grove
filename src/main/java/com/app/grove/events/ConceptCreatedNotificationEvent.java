fd6c5f5e1421f628a34019bc12e10856efd4171e
Maxwell <maxwellcollazossolis@gmail.com>
2026-05-24 11:29:07 -0500

    Agregando ConceptCreatedNotificationEvent

--- src/main/java/com/app/grove/events/ConceptCreatedNotificationEvent.java
+++ src/main/java/com/app/grove/events/ConceptCreatedNotificationEvent.java
@@ -1,4 +1,12 @@
 package com.app.grove.events;
 
+import lombok.Value;
+
+@Value
 public class ConceptCreatedNotificationEvent {
+    String conceptId;
+    String conceptTitle;
+    String workspaceId;
+    String createdByUserId;
+
 }
