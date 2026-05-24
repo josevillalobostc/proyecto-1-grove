accf7c8628b6f4bbd6838ebc35473e3a430f1ccf
rvcuba <rvl.cuba@gmail.com>
2026-05-24 13:32:37 -0500

    Versions Added to TagController

--- src/main/java/com/app/grove/tag/application/TagController.java
+++ src/main/java/com/app/grove/tag/application/TagController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tags")
+@RequestMapping("/api/v1/tags")
 @RequiredArgsConstructor
 public class TagController {
 
