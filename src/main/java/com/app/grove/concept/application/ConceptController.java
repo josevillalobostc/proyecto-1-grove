4c47b74d888c947dee1298c477ed381d24f00491
rvcuba <rvl.cuba@gmail.com>
2026-05-24 13:32:18 -0500

    Versions Added to ConceptController

--- src/main/java/com/app/grove/concept/application/ConceptController.java
+++ src/main/java/com/app/grove/concept/application/ConceptController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/concepts")
+@RequestMapping("/api/v1/concepts")
 @RequiredArgsConstructor
 public class ConceptController {
 
