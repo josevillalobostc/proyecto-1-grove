9d58c9723c05a32c22d2d605e3116df4fa4b7980
rvcuba <rvl.cuba@gmail.com>
2026-05-23 19:30:01 -0500

    DTO TagResponse Created

---
+++ src/main/java/com/app/grove/tag/dto/TagResponse.java
@@ -0,0 +1,12 @@
+package com.app.grove.tag.dto;
+
+import lombok.Data;
+
+@Data
+public class TagResponse {
+
+    private String id;
+    private String name;
+    private String description;
+    private String color;
+}
