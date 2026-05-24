3f57e0ef2ca619d77afce87b176ab8dee6402658
rvcuba <rvl.cuba@gmail.com>
2026-05-23 19:30:33 -0500

    DTO WorkspaceRequest Created

---
+++ src/main/java/com/app/grove/workspace/dto/WorkspaceRequest.java
@@ -0,0 +1,13 @@
+package com.app.grove.workspace.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class WorkspaceRequest {
+
+    @NotBlank(message = "El nombre del workspace es obligatorio")
+    private String name;
+
+    private String description;
+}
