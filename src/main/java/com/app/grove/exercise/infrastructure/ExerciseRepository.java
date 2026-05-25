ad531ba663a1824f82d0d469507299b66f9c35fe
Elías <elias.paz@utec.edu.pe>
2026-05-24 19:34:35 -0500

    Añadido ExerciseRepository

---
+++ src/main/java/com/app/grove/exercise/infrastructure/ExerciseRepository.java
@@ -0,0 +1,20 @@
+package com.app.grove.exercise.infrastructure;
+
+
+import com.app.grove.exercise.domain.Exercise;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExerciseRepository extends Neo4jRepository<Exercise,String> {
+    List<Exercise> findByType(String type);
+    List<Exercise> findByDifficulty(Integer difficulty);
+    List<Exercise> findByQuestionContaining(String keyword);
+
+    @Query("MATCH(e:Exercise)-[:BELONGS_TO]->(u:User) WHERE u.id=$user.Id RETURN e")
+    List<Exercise> findExerciseByUserId(String userId);
+
+}
