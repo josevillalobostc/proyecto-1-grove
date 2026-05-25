4f82d83433d9518d2948343ff0d485ea39fe7a36
rvcuba <rvl.cuba@gmail.com>
2026-05-25 14:20:19 -0500

    Rest Controller Annotation to User

--- src/main/java/com/app/grove/user/application/UserController.java
+++ src/main/java/com/app/grove/user/application/UserController.java
@@ -3,12 +3,14 @@ package com.app.grove.user.application;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.app.grove.user.domain.UserService;
 import com.app.grove.user.dto.UserResponse;
 
 import lombok.RequiredArgsConstructor;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
 public class UserController {
@@ -19,5 +21,5 @@ public class UserController {
         UserResponse response = userService.getUserInfo();
         return ResponseEntity.ok(response);
     }
-	
+
 }
