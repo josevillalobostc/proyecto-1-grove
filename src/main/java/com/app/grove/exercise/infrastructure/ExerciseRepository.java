90ea01f28191c6706a3a9c0834c321560b429e26
rvcuba <rvl.cuba@gmail.com>
2026-05-25 12:08:46 -0500

    Cypher fixed

--- src/main/java/com/app/grove/exercise/infrastructure/ExerciseRepository.java
+++ src/main/java/com/app/grove/exercise/infrastructure/ExerciseRepository.java
@@ -4,6 +4,7 @@ package com.app.grove.exercise.infrastructure;
 import com.app.grove.exercise.domain.Exercise;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public interface ExerciseRepository extends Neo4jRepository<Exercise,String> {
     List<Exercise> findByDifficulty(Integer difficulty);
     List<Exercise> findByQuestionContaining(String keyword);
 
-    @Query("MATCH(e:Exercise)-[:BELONGS_TO]->(u:User) WHERE u.id=$user.Id RETURN e")
-    List<Exercise> findExerciseByUserId(String userId);
+    @Query("MATCH(e:Exercise)-[:BELONGS_TO]->(u:User) WHERE u.id = $userId RETURN e")
+    List<Exercise> findExerciseByUserId(@Param("userId") String userId);
 
 }
