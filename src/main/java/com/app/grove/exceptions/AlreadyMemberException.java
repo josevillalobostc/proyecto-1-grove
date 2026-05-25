9ce8c1f36a97a3c612304b47ca91a9a7715b8fdd
rvcuba <rvl.cuba@gmail.com>
2026-05-25 15:43:05 -0500

    Global Exception Handler add exception

---
+++ src/main/java/com/app/grove/exceptions/AlreadyMemberException.java
@@ -0,0 +1,7 @@
+package com.app.grove.exceptions;
+
+public class AlreadyMemberException extends RuntimeException {
+    public AlreadyMemberException(String message) {
+        super(message);
+    }
+}
