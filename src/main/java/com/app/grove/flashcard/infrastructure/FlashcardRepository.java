825612bb62b7921533ef7a7e040e590968b58a1c
rvcuba <rvl.cuba@gmail.com>
2026-05-25 12:54:33 -0500

    Added Pageable to Flashcard Repository

--- src/main/java/com/app/grove/flashcard/infrastructure/FlashcardRepository.java
+++ src/main/java/com/app/grove/flashcard/infrastructure/FlashcardRepository.java
@@ -1,14 +1,13 @@
 package com.app.grove.flashcard.infrastructure;
 
-import com.app.grove.exercise.domain.Exercise;
 import com.app.grove.flashcard.domain.Flashcard;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface FlashcardRepository extends Neo4jRepository<Flashcard,String> {
-    List<Flashcard> findByDifficulty(Integer difficulty);
-
+    Page<Flashcard> findByDifficulty(Integer difficulty, Pageable pageable);
 }
