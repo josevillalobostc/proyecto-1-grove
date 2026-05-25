2b01d761ea814f7cb87bf2fc65c3d79f6b78e362
José Villalobos <jose.villalobos.j@utec.edu.pe>
2026-05-24 22:08:16 -0500

    Agregados imports en Flashcard.java

--- src/main/java/com/app/grove/flashcard/domain/Flashcard.java
+++ src/main/java/com/app/grove/flashcard/domain/Flashcard.java
@@ -9,7 +9,10 @@ import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
+import com.app.grove.concept.domain.Concept;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Node("Flashcard")
 @AllArgsConstructor
